8642e28a2e090b9552d4cdd3ff405fbf54b6d6c1
Gilson Oliveira <gilsgil@gmail.com>
2021-09-10 19:48:07 -0300

    projeto iniciado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import "./App.css";
-import Header from "./components/header/header";
-import HomePage from "./pages/homepage/homepage";
-import Alunos from "./pages/alunos/alunos";
-import Cadastro from "./pages/cadastro/cadastro";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 
 class App extends React.Component {
   render() {
-    return (
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact={true}>
-            <HomePage />
-          </Route>
-          <Route path="/alunos">
-            <Alunos />
-          </Route>
-          <Route path="/cadastro">
-            <Cadastro />
-          </Route>
-        </Switch>
-      </Router>
-    );
+    return <div></div>;
   }
 }
 
--- src/components/lista/lista.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Lista() {
-  return <div></div>;
-}
-
-export default Lista;
