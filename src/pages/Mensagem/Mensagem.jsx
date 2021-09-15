0678135ffc9168635005ea43fdf6300845746684
Gilson Oliveira <gilsgil@gmail.com>
2021-09-15 12:40:15 -0300

    fix de app quebrado

--- src/pages/Mensagem/Mensagem.jsx
+++ src/pages/Mensagem/Mensagem.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-<<<<<<< HEAD:src/components/pages/Mensagem/Mensagem.jsx
-import Mensagens from "../../Mensagens/Mensagens";
-=======
-import { Container } from "react-bootstrap";
-import Mensagens from "../../components/Mensagens/Mensagens"
->>>>>>> afd750e3b5406ed2603785b9a5355ba3b01b3885:src/pages/Mensagem/Mensagem.jsx
+import Mensagens from "../../components/Mensagens/Mensagens";
 import "./Mensagem.css";
 
 function Mensagem() {
